1129792e16de44e390d8db57206385534da01202
KS Chen <arcleaf@yahoo.com.tw>
2019-11-19 17:01:46 +0800

    to a PWA

---
+++ build/precache-manifest.d6cded92eac54012a95e503911de89e7.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84791de1f9f681fcd93a764ed6e4702d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "b2224567f856a606e43d",
+    "url": "/weather-app/static/css/main.e5f616a9.chunk.css"
+  },
+  {
+    "revision": "5a8bb2e7651288f3509a",
+    "url": "/weather-app/static/js/2.db796e2d.chunk.js"
+  },
+  {
+    "revision": "b2224567f856a606e43d",
+    "url": "/weather-app/static/js/main.d67afe7c.chunk.js"
+  },
+  {
+    "revision": "fbf68f987b3c8ea6ac98",
+    "url": "/weather-app/static/js/runtime-main.5adfc449.js"
+  },
+  {
+    "revision": "981a9625d6d19f18cc5c290ccb0ee41b",
+    "url": "/weather-app/static/media/air-flow.981a9625.svg"
+  },
+  {
+    "revision": "7ca9fe14f0863a8639463718f68ee69f",
+    "url": "/weather-app/static/media/cog.7ca9fe14.svg"
+  },
+  {
+    "revision": "19e2ef132632c2a41a547fd987ea4332",
+    "url": "/weather-app/static/media/day-clear.19e2ef13.svg"
+  },
+  {
+    "revision": "296112b168b03996ac80e9c7d324bd80",
+    "url": "/weather-app/static/media/day-cloudy-fog.296112b1.svg"
+  },
+  {
+    "revision": "5c66b1345ba4954b591a9abd473fec8e",
+    "url": "/weather-app/static/media/day-cloudy.5c66b134.svg"
+  },
+  {
+    "revision": "4b1ec37a80fcfeb3339628e1d941bd95",
+    "url": "/weather-app/static/media/day-fog.4b1ec37a.svg"
+  },
+  {
+    "revision": "51c7cd390b88854769659caf903cf233",
+    "url": "/weather-app/static/media/day-partially-clear-with-rain.51c7cd39.svg"
+  },
+  {
+    "revision": "dd8509241b9184ffc6498e27e256d12d",
+    "url": "/weather-app/static/media/day-snowing.dd850924.svg"
+  },
+  {
+    "revision": "32a450a8eb3d385943af76f3738204b1",
+    "url": "/weather-app/static/media/day-thunderstorm.32a450a8.svg"
+  },
+  {
+    "revision": "7efb85c9726be15177971c2aa0038d00",
+    "url": "/weather-app/static/media/easter-egg.7efb85c9.svg"
+  },
+  {
+    "revision": "bffa65e7f30ad0a23468893a58cf1445",
+    "url": "/weather-app/static/media/loading.bffa65e7.svg"
+  },
+  {
+    "revision": "a99547e9b50556379a96c369d2cd8a8c",
+    "url": "/weather-app/static/media/night-clear.a99547e9.svg"
+  },
+  {
+    "revision": "c78ef76288a695f6d4d010fd4441b8b7",
+    "url": "/weather-app/static/media/night-cloudy-fog.c78ef762.svg"
+  },
+  {
+    "revision": "3f25314886812bb140ce00c75ce38df7",
+    "url": "/weather-app/static/media/night-cloudy.3f253148.svg"
+  },
+  {
+    "revision": "2477d0a9b2c5d67dc8b806ff6dc30d42",
+    "url": "/weather-app/static/media/night-fog.2477d0a9.svg"
+  },
+  {
+    "revision": "3a37b82a0663f3627389043001c0c190",
+    "url": "/weather-app/static/media/night-partially-clear-with-rain.3a37b82a.svg"
+  },
+  {
+    "revision": "19cb9f62d708528aa8145d69d7d1c713",
+    "url": "/weather-app/static/media/night-snowing.19cb9f62.svg"
+  },
+  {
+    "revision": "5f007c776d1d327f40da690c8014a9d8",
+    "url": "/weather-app/static/media/night-thunderstorm.5f007c77.svg"
+  },
+  {
+    "revision": "881734c06e6bbfd0addb6e1459f25d60",
+    "url": "/weather-app/static/media/rain.881734c0.svg"
+  },
+  {
+    "revision": "ed958a11665374f2c6b0ce938a469a9c",
+    "url": "/weather-app/static/media/refresh.ed958a11.svg"
+  }
+]);
